01bdb35c4b7ecec904edcfe94643314675c26fb6
Treikeh <73736974+Treikeh@users.noreply.github.com>
2024-10-22 13:11:23 +0200

    Hello it's me

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    private void Start()
+    {
+        // 
+    }
+
+    private void Update()
+    {
+        //
+    }
+
+    private void FixedUpdate()
+    {
+        //
+    }
+
+    private void CheckForInteractable()
+    {
+        //
+    }
+
+    private void CheckForAttackable()
+    {
+        //
+    }
+
+    public void OnLook(InputValue inputValue)
+    {
+        // Look Input
+    }
+
+    public void OnMove(InputValue inputValue)
+    {
+        // Move Input
+    }
+
+    public void OnJmup()
+    {
+        //
+    }
+
+    public void OnInteract()
+    {
+        // Interact Input
+    }
+
+    public void OnAttack()
+    {
+        // Attack Input
+    }
+}
