8d3c73c3bd57da9801ad66a963d86c63e618048c
Treikeh <73736974+Treikeh@users.noreply.github.com>
2024-11-11 10:27:27 +0100

    Had a few issues with lighting in level 3 and started working on the intro dialog.

--- Assets/Scripts/Ui/MainMenu.cs
+++ Assets/Scripts/Ui/MainMenu.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,12 +10,33 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private float fadeDuration = 0.5f;
     [SerializeField] private float fadeBuffer = 0.5f;
     [SerializeField] private Image fadePanel;
+    [SerializeField] private GameObject mainMenuMenu;
+    [SerializeField] private GameObject levelSelect;
+    [SerializeField] private GameObject introDialog;
+
+    // Intro dialog
+    [SerializeField] private List<string> xiyuanDialog;
+    [SerializeField] private List<string> lixinDialog;
+    [SerializeField] private TMP_Text xiyuanText;
+    [SerializeField] private TMP_Text lixinText;
+    private bool dialogActive = false;
+    private int xiyuanDialogIndex = 0;
+    private int lixinDialogIndex = 0;
+
 
 
     private void Start()
     {
         fadePanel.gameObject.SetActive(true);
         Invoke(nameof(FadeOutPanel), fadeBuffer);
+    }
+
+    private void Update()
+    {
+        if (dialogActive)
+        {
+            // Change dialog lines
+        }
     }
 
     // INPUTS //
@@ -26,6 +49,23 @@ public class MainMenu : MonoBehaviour
     public void OnQuitButtonPressed()
     {
         StartCoroutine(QuitGameFade());
+    }
+
+    public void OnLevelSelectButtonPressed()
+    {
+        mainMenuMenu.SetActive(false);
+        levelSelect.SetActive(true);
+    }
+
+    public void OnBackButtonPressed()
+    {
+        mainMenuMenu.SetActive(true);
+        levelSelect.SetActive(false);
+    }
+
+    public void OnLevelButtonPressed(int level)
+    {
+        StartCoroutine(LevelButtonsFade(level));
     }
 
     // FADE INN AND FADE OUT //
@@ -47,6 +87,24 @@ public class MainMenu : MonoBehaviour
     {
         FadeInnPanel();
         yield return new WaitForSecondsRealtime(fadeDuration + fadeBuffer);
+        mainMenuMenu.SetActive(false);
+        StartCoroutine(DialogStartFade());
+    }
+
+    private IEnumerator DialogStartFade()
+    {
+        FadeOutPanel();
+        introDialog.SetActive(true);
+        xiyuanText.text = xiyuanDialog[xiyuanDialogIndex];
+        lixinText.text = lixinDialog[lixinDialogIndex];
+        yield return new WaitForSecondsRealtime(fadeDuration + fadeBuffer);
+        dialogActive = true;
+    }
+
+    private IEnumerator DialogEndFade()
+    {
+        FadeInnPanel();
+        yield return new WaitForSecondsRealtime(fadeDuration + fadeBuffer);
         SceneLoader.LoadNextBuildScene();
     }
 
@@ -55,5 +113,26 @@ public class MainMenu : MonoBehaviour
         FadeInnPanel();
         yield return new WaitForSecondsRealtime(fadeDuration + fadeBuffer);
         Application.Quit();
+    }
+
+    private IEnumerator LevelButtonsFade(int level)
+    {
+        FadeInnPanel();
+        yield return new WaitForSecondsRealtime(fadeDuration + fadeBuffer);
+        switch (level) {
+            case 1:
+                SceneLoader.LoadSceneByName("Level 1");
+                break;
+            case 2:
+                SceneLoader.LoadSceneByName("Level 2");
+                break;
+            case 3:
+                SceneLoader.LoadSceneByName("Level 3");
+                break;
+            default:
+                Debug.Log("Enter level number (1 - 3)");
+                FadeOutPanel();
+                break;
+        }
     }
 }
