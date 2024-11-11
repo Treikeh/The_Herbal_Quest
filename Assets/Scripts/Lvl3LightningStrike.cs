8556bfe167f2374f923a4a699605e03b906ea79a
Treikeh <73736974+Treikeh@users.noreply.github.com>
2024-11-11 15:10:55 +0100

    Worked on level 3

--- Assets/Scripts/Lvl3LightningStrike.cs
+++ Assets/Scripts/Lvl3LightningStrike.cs
@@ -6,16 +6,26 @@ using UnityEngine.UI;
 
 public class Lvl3LightningStrike : MonoBehaviour
 {
-    [SerializeField] private GameObject lightningImage;
+    [SerializeField] private Material lightningMaterial;
     [SerializeField] private Light lightningLight;
 
-    private void Start()
+    private bool triggerd = false;
+
+    private void Update()
     {
-        lightningImage.SetActive(false);
+        if (triggerd)
+        {
+            var temp = lightningMaterial.color;
+            temp.a -= Time.deltaTime / 1.5f;
+            lightningLight.intensity -= Time.deltaTime * 30f;
+            lightningMaterial.color = temp;
+        }
     }
 
     public void TriggerLightningStrike()
     {
-        lightningImage.SetActive(true);
+        lightningMaterial.color = Color.white;
+        lightningLight.intensity = 40f;
+        triggerd = true;
     }
 }
