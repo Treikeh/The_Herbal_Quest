8cabddf1b86666ac2ddd42f856aa12c08601afca
Treikeh <73736974+Treikeh@users.noreply.github.com>
2024-11-12 18:12:33 +0100

    Had a few issues with the credits, they are fixed now

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Credits : MonoBehaviour
 {
     [SerializeField] private GameObject mainCredits;
     [SerializeField] private GameObject assetsCredits;
+
+    [SerializeField] private float fadeDuration = 0.5f;
+    [SerializeField] private float fadeBuffer = 0.5f;
+    [SerializeField] private Image fadePanel;
 
     public void OnAssetCreditsButtonPressed()
     {
@@ -17,5 +22,24 @@ public class Credits : MonoBehaviour
     {
         mainCredits.SetActive(true);
         assetsCredits.SetActive(false);
+    }
+
+    public void OnMainMenuButtonPressed()
+    {
+        StartCoroutine(MainMenuFade());
+    }
+
+    private void FadePanelInn()
+    {
+        // CrossFadeAlpha does not work without this work around
+        fadePanel.CrossFadeAlpha(0f, 0f, true);
+        fadePanel.CrossFadeAlpha(1f, fadeDuration, true);
+    }
+
+    private IEnumerator MainMenuFade()
+    {
+        FadePanelInn();
+        yield return new WaitForSecondsRealtime(fadeDuration + fadeBuffer);
+        SceneLoader.LoadSceneByName("MainMenu");
     }
 }
